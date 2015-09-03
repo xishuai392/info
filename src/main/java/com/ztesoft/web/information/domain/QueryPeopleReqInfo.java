e19c323e335c7323f3d1f021a6de2ef7e0c5fc9e
felicity <2324063702@qq.com>
2015-09-03 13:47:18 +0800

    felicity demo

---
+++ src/main/java/com/ztesoft/web/information/domain/QueryPeopleReqInfo.java
@@ -0,0 +1,69 @@
+package com.ztesoft.web.information.domain;
+/**
+ * 查询人请求对象信息
+ * 
+查询申请人信息
+
+证件号：
+
+证件类型：
+
+下一步
+
+查询申请人类型：
+
+申请查询人单位：
+
+姓名：
+
+查询事由：
+
+ * @author Ocean
+ *
+ */
+public class QueryPeopleReqInfo {
+	private String idCardNum;
+	private String indentyType;
+	private String name;
+	private String applicantQueryType;
+	private String applicantCompany;
+	private String queryResult;
+	private String cxbs;
+	public String getIdCardNum() {
+		return idCardNum;
+	}
+	public void setIdCardNum(String idCardNum) {
+		this.idCardNum = idCardNum;
+	}
+	public String getIndentyType() {
+		return indentyType;
+	}
+	public void setIndentyType(String indentyType) {
+		this.indentyType = indentyType;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getApplicantQueryType() {
+		return applicantQueryType;
+	}
+	public void setApplicantQueryType(String applicantQueryType) {
+		this.applicantQueryType = applicantQueryType;
+	}
+	public String getApplicantCompany() {
+		return applicantCompany;
+	}
+	public void setApplicantCompany(String applicantCompany) {
+		this.applicantCompany = applicantCompany;
+	}
+	public String getQueryResult() {
+		return queryResult;
+	}
+	public void setQueryResult(String queryResult) {
+		this.queryResult = queryResult;
+	}
+	
+}
