43bb9ede761340f6d6feb9730b8bb944aa900355
felicity <2324063702@qq.com>
2015-09-03 16:05:43 +0800

    ee

--- src/main/java/com/ztesoft/web/information/domain/req/scan/FileScanReqInfo.java
+++ src/main/java/com/ztesoft/web/information/domain/req/scan/FileScanReqInfo.java
@@ -5,5 +5,29 @@ package com.ztesoft.web.information.domain.req.scan;
  *
  */
 public class FileScanReqInfo {
-
+	//文件日志记录表的uuid
+	private String cxrzId;
+	//文件名
+	private String fileName;
+	//
+	private String filePath;
+	public String getCxrzId() {
+		return cxrzId;
+	}
+	public void setCxrzId(String cxrzId) {
+		this.cxrzId = cxrzId;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	
 }
--- src/main/java/com/ztesoft/web/information/domain/req/scan/IdScanReqInfo.java
+++ src/main/java/com/ztesoft/web/information/domain/req/scan/IdScanReqInfo.java
@@ -1,5 +1,30 @@
 package com.ztesoft.web.information.domain.req.scan;
 
 public class IdScanReqInfo {
-
+	//文件日志记录表的uuid
+	private String cxrzId;
+	//文件名
+	private String fileName;
+	//文件存放路径
+	private String filePath;
+	public String getCxrzId() {
+		return cxrzId;
+	}
+	public void setCxrzId(String cxrzId) {
+		this.cxrzId = cxrzId;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	
+	
 }
